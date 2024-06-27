a0703bc040ec4735b42a8dfae3dadc2d04a9d76d
Jman <evgenrevin@gmail.com>
2024-06-27 21:57:10 +0300

    new

--- Codewars_training.py
+++ Codewars_training.py
@@ -20,17 +20,71 @@ the 2nd argument is found in the first one.
 
 If no occurrences can be found, a count of 0 should be returned."""
 # V1
-def str_count(strng, letter):
-    if not strng:
-        return 0
-    counter = 0
-    for char in strng:
-        if char == letter:
-            counter += 1
-    return counter
-print(str_count("Hello", 'o'))
+# def str_count(strng, letter):
+#     if not strng:
+#         return 0
+#     counter = 0
+#     for char in strng:
+#         if char == letter:
+#             counter += 1
+#     return counter
+# print(str_count("Hello", 'o'))
 
 # V2
-def str_count(strng, letter):
-    return strng.count(letter)
-print(str_count("Hello", 'o'))
+# def str_count(strng, letter):
+#     return strng.count(letter)
+# print(str_count("Hello", 'o'))
+
+"""Create a combat function that takes the player's current health and the amount of damage recieved, 
+and returns the player's new health. Health can't be less than 0."""
+
+# V1
+# def combat(health, damage):
+#     result = health - damage
+#     if result > 0:
+#         return result
+#     else:
+#         return 0
+# print(combat(83, 16))
+# # V2
+# def combat(health, damage):
+#     return max(0, health-damage)
+# print(combat(83, 16))
+
+"""Trolls are attacking your comment section!
+A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+Your task is to write a function that takes a string and return a new string with all vowels removed.
+For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+Note: for this kata y isn't considered a vowel."""
+# V1
+# def disemvowel(string_):
+#     char_to_remome = "aoeiuAOEIU"
+#     new_string = ""
+#     for char in string_:
+#         if char not in char_to_remome:
+#             new_string += char
+#     return new_string
+# print(disemvowel("Ayy common way to deal with this situation is to remove all of the vowels"))
+# V2
+# def disemvowel(string):
+#     return "".join(c for c in string if c.lower() not in "aeiou")
+# print(disemvowel("Ayy common way to deal with this situation is to remove all of the vowels"))
+
+"""In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number."""
+
+# V1
+def high_and_low(numbers):
+    list_of_num = list(map(int, numbers.split())) # Розділяємо рядок на список чисел і конвертуємо кожен елемент у ціле число
+    min_num = min(list_of_num)
+    max_num = max(list_of_num)
+    result = f'{max_num} {min_num}'
+    return result
+
+print(high_and_low("8 3 -5 42 -1 0 0 -9 4 7 4 -4"))
+
+# V2
+def high_and_low(numbers):
+  numbers = [int(c) for c in numbers.split(' ')]
+  return f"{max(numbers)} {min(numbers)}"
+
+print(high_and_low("8 3 -5 42 -1 0 0 -9 4 7 4 -4"))
